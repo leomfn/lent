4c157bb2f38b454a35bcb33c6611302f4ea4fc31
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2024-02-20 00:07:26 +0100

    make time zones appear as defined in TIME_ZONE env variable

--- src/config/settings.py
+++ src/config/settings.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = os.environ["LANGUAGE_CODE"]
 
-TIME_ZONE = "UTC"
+TIME_ZONE = os.environ["TIME_ZONE"]
 
 USE_I18N = True
 
