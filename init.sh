1ac54c66b6ccdf94dfd4bfd5cb9b3f7f2504e213
Leo Fuhrmann <99895548+leomfn@users.noreply.github.com>
2024-02-26 21:46:03 +0100

    13 use package manager and bundler to include necessary js and css files (#14)
    * use webpack bundler and npm package manager
    
    * add example .env file

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if [ ! -f "${DB_FILE_PATH}/db.sqlite3" ]; then
+    echo "No database file was found. This probably means that you have started lent for the first time."
+    echo "Creating database"
+    python manage.py migrate
+    echo "Creating initial superuser with provided credentials"
+    DJANGO_SUPERUSER_PASSWORD=${DJANGO_ADMIN_PASSWORD} python manage.py createsuperuser --username "$DJANGO_ADMIN_USERNAME" --email "$DJANGO_ADMIN_EMAIL" --no-input
+fi
+
 if [ "$DEBUG" = 'OFF' ]; then
     echo "Collecting static files"
     python manage.py collectstatic --no-input
